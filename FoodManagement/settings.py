44cd1077b153bb4a88246f079fb7067a7930ed10
Ashish <ashishsn3301@gmail.com>
2026-09-20 01:14:13 +0530

    Test GitOps deployment

--- FoodManagement/settings.py
+++ FoodManagement/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+# GitOps deployment test
 
 from pathlib import Path
 import os
